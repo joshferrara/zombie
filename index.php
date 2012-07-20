db73f396b514bf39130ffc252ed54fb36faf1431
Josh Ferrara <josh@hivemindlabs.com>
2012-07-19 23:02:06 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head profile="http://www.w3.org/2005/10/profile">
+<link rel="icon" 
+      type="image/png" 
+      href="/images/favicon.png" /> 
+
+	<title>Zombie &mdash; It's Alive!</title>
+	<meta name="description" content="Try not to use more than 15-20 words. With the site clearly described in the first 8-10.">
+	<meta name="keywords" content="Comma-separated: keyword1, keyword2, etc. Do NOT force keywords. These words MUST have some density in the site content. Use 8-10.">
+	
+	<!-- Make it pretty -->
+	<link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" title="main" charset="utf-8">
+	<link rel="shortcut icon" href="/folder-name/logo.ico">
+	
+	<!-- Even for Internet Explorer -->
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
+	<!--[if IE 7]>
+		<link rel="stylesheet" href="styles/ie7.css" type="text/css" media="screen" title="ie7" charset="utf-8">
+	<![endif]-->
+
+	<!-- Make it behave -->
+	<script src="scripts/jquery.js" type="text/javascript" charset="utf-8">
+		// version 1.4.2
+	</script>
+	<script src="scripts/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>
+	
+	<!-- Warn IE6 Users -->
+	<!--[if LTE IE 6]>
+		<link rel="stylesheet" href="http://gatorworks.net/ie6warning/ie6.css" type="text/css" media="screen" title="ie6" charset="utf-8">
+		<script src="http://gatorworks.net/ie6warning/ie6warning.js" type="text/javascript" charset="utf-8"></script>
+	<![endif]-->
+</head>
+<body>
+
+</body>
+</html>
